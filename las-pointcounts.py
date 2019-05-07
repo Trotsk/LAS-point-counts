8ab26a27cd35b85c1299f609ae37ee1ff47ac153
Trotsk <nic.chandler@gmail.com>
2019-05-06 23:14:51 -0700

    additional comments + pep8 convention

--- las-pointcounts.py
+++ las-pointcounts.py
@@ -1,3 +1,13 @@
+"""Computes point counts for all size datasets.
+
+Resolves issue with LASinfo's -merge command which is restricted
+by memory constraints and has no in-built error handling -
+it will spit out a result and then terminate.
+
+Requires Python 3.x
+Requires LAStools downloaded into suggested data structure (C:\LAStools\bin)
+"""
+
 import re
 from glob import glob
 import subprocess
@@ -7,38 +17,42 @@ from pprint import pprint
 # I know global variables are unpythonic, I'll fix it later
 g_partition = "histogram of classification of points:"
 
+
 def extract_classes(text):
-  """ Intakes text string, returns point classifications as key-value pair """
-  raw_text = text.partition(g_partition)
-  text = raw_text[2]
-  clean_lines = [l.lstrip() for l in text.splitlines()]
-  parse_lines = [l for l in clean_lines if re.match(r"^\d+.*$",l)]
-  return {key: int(val) for val, key in (parse_lines.split(" ",1) for parse_lines in parse_lines)}
+    """Intakes text string, returns point classifications as key-value pair"""
+    raw_text = text.partition(g_partition)
+    text = raw_text[2]
+    clean_lines = [l.lstrip() for l in text.splitlines()]
+    parse_lines = [l for l in clean_lines if re.match(r"^\d+.*$", l)]
+    return {key: int(val) for val, key in (parse_lines.split(" ", 1)
+            for parse_lines in parse_lines)}
 
 
 def main():
-  """ Invokes lasinfo, parses output for each LAS file, creates dictionary and updates 
-  point counts for each successive loop """
-  start_time = time.time()
-  filename = glob("*.las") # searches all LAS files in directory
-  dict = {} # instantiate master dictionary
-  for i in filename:
-    # By default all output of lasinfo goes to stderr. A command argument redirects to stdout
-    proc = subprocess.Popen(['lasinfo', i, '-no_vlrs', '-stdout'], stdout = subprocess.PIPE)
-    out = proc.communicate()[0].decode('utf-8').strip()
-    f = extract_classes(out)
-    for key, val in f.items():
-      if key in dict.keys():
-        dict[key] += val # if key already in dict, values updated
-      else:
-        dict[key] = val # key-value pair added to dict if key does not yet exist
-  elapsed_time = time.time() - start_time
-  print("\n")
-  print(g_partition)
-  print("\n")
-  pprint(dict)
-  print("\n")
-  print(time.strftime("Time elapsed: %H:%M:%S", time.gmtime(elapsed_time)))
-  
+    """ Invokes lasinfo, parses output for each LAS file,
+    creates dictionary and updates point counts for each successive loop """
+    start_time = time.time()
+    filename = glob("*.las")  # searches all LAS files in directory
+    dict = {}  # instantiate master dictionary
+    for i in filename:
+        # By default all output of lasinfo goes to stderr
+        # A command argument redirects to stdout
+        proc = subprocess.Popen(['lasinfo', i, '-no_vlrs', '-stdout'],
+                                stdout=subprocess.PIPE)
+        out = proc.communicate()[0].decode('utf-8').strip()
+        f = extract_classes(out)
+        for key, val in f.items():
+            if key in dict.keys():
+                dict[key] += val  # if key already in dict, values updated
+            else:
+                dict[key] = val  # unused key has key-value pair added to dict
+    elapsed_time = time.time() - start_time
+    print("\n")
+    print(g_partition)
+    print("\n")
+    pprint(dict)
+    print("\n")
+    print(time.strftime("Time elapsed: %H:%M:%S", time.gmtime(elapsed_time)))
+
 if __name__ == "__main__":
-  main()
+    main()
